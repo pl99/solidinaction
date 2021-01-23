94bc66e94606797830851bf469c5fb988c78270c
kotin <pavel.kotin@gmail.com>
2021-01-23 10:43:06 +0300

    initial commit

---
+++ src/main/java/ru/advantum/solidinaction/SolidinactionApplication.java
@@ -0,0 +1,13 @@
+package ru.advantum.solidinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidinactionApplication.class, args);
+    }
+
+}

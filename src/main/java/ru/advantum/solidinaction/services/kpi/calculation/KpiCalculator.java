72db0da26b7b08c37b506bab206d481c2a171c17
kotin <pavel.kotin@gmail.com>
2021-02-10 13:32:01 +0300

    расчет KPI - без изысков

--- src/main/java/ru/advantum/solidinaction/services/kpi/calculation/KpiCalculator.java
+++ src/main/java/ru/advantum/solidinaction/services/kpi/calculation/KpiCalculator.java
@@ -1,11 +1,8 @@
 package ru.advantum.solidinaction.services.kpi.calculation;
 
-import io.vavr.Tuple2;
-
 import java.math.BigDecimal;
 import java.util.AbstractMap;
 
 public interface KpiCalculator {
-
     AbstractMap.SimpleEntry<String, BigDecimal> calculate();
 }
